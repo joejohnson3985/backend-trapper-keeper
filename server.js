4363e4041c8d83fba1f138888596e213b1a35549
Duy Vu <duyv.thanh@gmail.com>
2019-05-22 14:04:32 -0600

    Separated list items and cards
    Co-authored-by:Joe <johnson.joseph527@gmail.com>
    Co-authored-by:Ryan <conner2468@gmail.com>

--- server.js
+++ server.js
@@ -7,25 +7,38 @@ const express = require('express');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.set('port', process.env.PORT || 3000);
 
 app.locals.title = 'Trapper Keeper'
 app.locals.cards = [
-    {id: 1, name: 'Grocery List', list: [
-        {list_id: 'a1', body: 'Soda Pop'},
-        {list_id: 'a2', body: 'Candy Canes'}
-    ]
-    },
-    {id: 2, name: 'Chores List', list: [
-        {list_id: 'b1', body: 'Do the dishes'}
-    ] 
-    }
+    {id: 1, name: 'Grocery List'},
+    {id: 2, name: 'Chores List'}
 ]
 
-app.use(express.json());
+app.locals.listItems = [
+    {list_id: 123123, card_id: 1, item: 'Soda pop'},
+    {list_id: 243432, card_id: 2, item: 'Do the dishes'}
+]
+
 
 app.get('/api/v1/cards', (request, response) => {
     return response.status(200).json(app.locals.cards)
+});
+
+app.get('/api/v1/cards/listitems', (req, res) => {
+    return res.status(200).json(app.locals.listItems)
+});
+
+app.post('/api/v1/cards', (req, res) => {
+    const { name, list } = req.body;
+    if(!name) return res.status(422).json('Please name your card')
+    const newCard = {
+        id: Date.now(),
+        ...req.body
+    };
+    app.locals.cards = [...app.locals.cards, newCard]
+    res.status(201).json(newCard)
 });
 
 
